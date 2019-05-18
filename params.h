035f856b6ba3de7e9d0fe628eb846157b0b9de8b
ashtul <44112901+ashtul@users.noreply.github.com>
2019-05-18 10:57:08 +0300

    Update params.h
    Tiny fix

--- params.h
+++ params.h
@@ -4,6 +4,6 @@
 #define N 1000000  // maximum flow
 #define M 1000000  // maximum size of stream-summary or CSS
 #define MAX_MEM 1000000 // maximum memory size
-#define HK_d 2 // maximum memory size
+#define HK_d 2 // Heavy Keeper depth
 
 #endif //_PARAMS_H
